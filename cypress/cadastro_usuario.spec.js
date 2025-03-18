b65a3ba980bdc2139dcb8c45305f6c786bc15bef
thiag <thiago.arica@outlook.com>
2025-03-18 15:38:42 -0300

    atualização referente criuar nomes aleatorios atraves do faker

--- cypress/cadastro_usuario.spec.js
+++ cypress/cadastro_usuario.spec.js
@@ -1,18 +1,24 @@
-describe("Teste de Cadastro", () => {
-  beforeEach(() =>{
-    cy.visit('/register');
+import { faker } from '@faker-js/faker';
 
-  })
+describe("Teste de Cadastro", () => {
+  beforeEach(() => {
+    cy.visit('/register');
+  });
+
   it("Deve realizar um cadastro válido", () => {
+    const name = faker.person.fullName();
+    const email = faker.internet.email();
+    const password = faker.internet.password(8); 
+
     cy.screenshot('01-acessando-pagina');
 
-    cy.get('#user').type("Thiago");
+    cy.get('#user').type(name);
     cy.screenshot('02-preenchendo-user');
 
-    cy.get('#email').type("thiago.arica@outlook.com");
+    cy.get('#email').type(email);
     cy.screenshot('03-preenchendo-email');
 
-    cy.get('#password').type("123456");
+    cy.get('#password').type(password);
     cy.screenshot('04-preenchendo-senha');
 
     cy.get('#btnRegister').click();
@@ -20,7 +26,7 @@ describe("Teste de Cadastro", () => {
 
     cy.get('.swal2-container').should('be.visible');
     cy.get('.swal2-title').should('have.text', 'Cadastro realizado!');
-    cy.get('.swal2-container').contains('Bem-vindo Thiago');
+    cy.get('.swal2-container').contains(`Bem-vindo ${name}`);
 
     cy.url().should('eq', 'https://automationpratice.com.br/my-account');
   });
